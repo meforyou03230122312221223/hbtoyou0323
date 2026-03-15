676787a9d51cf9456eed2413bafe3ac555dd3397
meforyou03230122312221223 <me.foryou.0323@gmail.com>
2026-03-15 22:42:38 +0530

    Update hb.js

--- hb/hb.js
+++ hb/hb.js
@@ -3,10 +3,11 @@ const cDown = document.getElementById("countdown");
 const balloonWrap = document.getElementById("balloon-container");
 
 const now = new Date();
-let target = new Date(now.getFullYear(), 2, 23, 0, -15, 0);
+
+let target = new Date(now.getFullYear(), 2, 22, 23, 45, 0);
 
 if (now > target) {
-    target = new Date(now.getFullYear() + 1, 2, 23, 0, -15, 0);
+    target = new Date(now.getFullYear() + 1, 2, 22, 23, 45, 0);
 }
 
 function spawnBalloon() {
